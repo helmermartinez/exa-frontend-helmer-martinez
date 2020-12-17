5bc9340eaa0774786a19d8e62e1ff110e9269a1a
helmermatinez <hlmermartinez@upeu.edu.pe>
2020-12-17 15:47:40 -0600

    Front End

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductoComponent } from './components/producto/producto.component';
+import { VentasComponent } from './components/ventas/ventas.component';
+import { LoginComponent } from './componets/login/login.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'login', component:LoginComponent},
+  {path: 'producto', component: ProductoComponent},
+  {path: 'ventas', component: VentasComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,43 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './componets/login/login.component';
+import { RolComponent } from './componets/rol/rol.component';
+import { MenuComponent } from './componets/menu/menu.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { RolService } from './services/rol.service';
+import { AuthService } from './services/auth.service';
+import { InterceptorService } from './interceptor/interceptor.service';
+import { VentasComponent } from './components/ventas/ventas.component';
+import { ProductoComponent } from './components/producto/producto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RolComponent,
+    MenuComponent,
+    VentasComponent,
+    ProductoComponent
   ],
   imports: [
+    CommonModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    RolService,AuthService,
+    {
+      provide:HTTP_INTERCEPTORS,
+      useClass: InterceptorService,
+      multi:true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
